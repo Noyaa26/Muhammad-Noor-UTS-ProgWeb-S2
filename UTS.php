e30abf99a2c00ebb7e36151ec780f434094934bb
Muhammad Noor <mhmmdnoyz@gmail.com>
2025-05-06 10:46:11 +0800

    Add files via upload

--- UTS.php
+++ UTS.php
@@ -1,9 +1,8 @@
 <?php
-// Cek apakah parameter 'nama' dikirim melalui URL
-if (isset($_GET['nama'])) {
-    $nama = htmlspecialchars($_GET['nama']); // Menghindari XSS
-    echo "Halo, $nama!";
+if (isset($_POST['nama'])) {
+    $nama = $_POST['nama'];
+    echo "Halo " . $nama;
 } else {
-    echo "Halo, Muhammad Noor!";
+    echo "Muhammad Noor";
 }
 ?>
